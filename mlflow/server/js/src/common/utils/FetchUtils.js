31cfd3da6250cabe0c54fb76fe11815feb989354
Oleg Kusov <olegkusov@icloud.com>
2023-02-14 00:29:11 +0300

    fix: move iam url to env

--- mlflow/server/js/src/common/utils/FetchUtils.js
+++ mlflow/server/js/src/common/utils/FetchUtils.js
@@ -195,7 +195,7 @@ export const retry = async (
 
 const AUTH_ERROR_CODE = 403;
 
-const getTokenUrl = () => process.env.NODE_ENV === 'production' ? `${process.env.IAM_HOST}/iam-api/get_token` : '/iam-api/get_token';
+const getTokenUrl = () => process.env.NODE_ENV === 'production' ? `${process.env.IAM_HOST}${process.env.IAM_GET_TOKEN_URL}` : process.env.IAM_GET_TOKEN_URL;
 
 export const getToken = () => {
   return new Promise((resolve, reject) => retry(
